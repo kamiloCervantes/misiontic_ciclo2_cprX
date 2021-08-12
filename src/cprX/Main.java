75458a8a61ee54b9f0ffb3b296512be3ab72a3c8
Camilo Cervantes <cacesa8931@gmail.com>
2021-08-12 16:38:01 -0500

    código componente práctico 03

--- src/cprX/Main.java
+++ src/cprX/Main.java
@@ -8,54 +8,53 @@ public class Main {
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Ingrese el número de productos");
-		int num_productos = Integer.parseInt(input.nextLine());
-
-		int[] codigos = new int[num_productos];
-		int[] cant_bodega = new int[num_productos];
-		int[] cant_minima = new int[num_productos];
+		System.out.println("Ingrese la lista de códigos de producto. Ej. 354 256 127");
+		String[] codigos_productos = input.nextLine().split(" ");
 		
-		
-		
-		int max_cant = 0;
-		int min_cant = 1000000000;
-		int max_pos = 0;
-		int min_pos = 0;
-		
-		for(int i = 0; i<num_productos; i++) {
-			System.out.println("Ingrese el valor del codigo");
-			codigos[i] = input.nextInt();
+		System.out.println("Ingrese la tabla de cantidades en bodega. Ej. 17 34 60;14 31 65;45 2 12;56 43 8");
+		String[] cantidades_bodega = input.nextLine().split(";");
+		String[][] matriz_cantidades_bodega = new String[4][codigos_productos.length];
+		for(int i = 0; i< cantidades_bodega.length; i++) {
+			matriz_cantidades_bodega[i] = cantidades_bodega[i].split(" ");
 		}
 		
-		for(int i = 0; i<num_productos; i++) {
-			System.out.println("Ingrese el valor de la cantidad de la bodega");
-			cant_bodega[i] = input.nextInt();
+		System.out.println("Ingrese la tabla de cantidades minimas requeridas. Ej. 14 44 76;10 51 15;46 23 2;89 4 18");
+		String[] cantidades_minimas = input.nextLine().split(";");
+		String[][] matriz_cantidades_minimas = new String[4][codigos_productos.length];
+		for(int j = 0; j< cantidades_minimas.length; j++) {
+			matriz_cantidades_minimas[j] = cantidades_minimas[j].split(" ");
 		}
 		
-		for(int i = 0; i<num_productos; i++) {
-			System.out.println("Ingrese el valor de la cantidad minima");
-			cant_minima[i] = input.nextInt();
-		}
-		
-		System.out.println("Códigos de productos que son necesario pedir");
-		for(int i = 0; i<num_productos; i++) {			
-			if(cant_bodega[i] < cant_minima[i]) {
-				System.out.println(codigos[i]); 
-			}
-			
-			if(cant_bodega[i] > max_cant) {
-				max_cant = cant_bodega[i];
-				max_pos = i;
-			}
-			
-			if(cant_bodega[i] < min_cant) {
-				min_cant = cant_bodega[i];
-				min_pos = i;
+		//Se debe solicitar producto en ...
+		for(int m = 0; m < 4; m++) {
+			for(int n = 0; n < codigos_productos.length; n++) {
+				String producto = codigos_productos[n];
+				if(Integer.parseInt(matriz_cantidades_bodega[m][n]) < Integer.parseInt(matriz_cantidades_minimas[m][n])) {
+					System.out.println("Se debe solicitar producto "+producto+" en sede "+m);
+				}
 			}
 		}
 		
-		System.out.println("Código con mayor número unidades:" + codigos[max_pos]);
-		System.out.println("Código con menor número unidades:" + codigos[min_pos]);
+		//Promedio de productos ...
+		double[] promedios = new double[codigos_productos.length];
+		for(int m = 0; m < 4; m++) {
+			for(int n = 0; n < codigos_productos.length; n++) {
+				promedios[n] = promedios[n] + Integer.parseInt(matriz_cantidades_bodega[m][n]);
+			}
+		}
+		
+		for(int d = 0; d < promedios.length; d++) {
+			System.out.println("El promedio de productos del codigo "+codigos_productos[d]+" es "+promedios[d]/4);
+		}
+		
+		/*
+		 * Nota: Los promedios son diferentes a los de la lección dado que me parece
+		 * que la forma correcta de sacarlos es sumando la cantidad del producto en
+		 * bodega de cada sede y dividiendo entre 4. En el componente práctico no se
+		 * sabe como sacaron esos promedios pero no corresponde con lo que debería ser.
+		 */
+		
+		
 		
 		input.close();
 	}
