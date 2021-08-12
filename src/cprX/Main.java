c864287dd9debaafaf6bd975e585b9f8cfbbaa7d
Camilo Cervantes <cacesa8931@gmail.com>
2021-08-12 17:19:42 -0500

    código componente práctico 05

--- src/cprX/Main.java
+++ src/cprX/Main.java
@@ -8,13 +8,34 @@ public class Main {
 		
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Ingrese los datos del producto (Código, precio, cantidad bodega, cantidad mínima");
-		String[] data_producto = input.nextLine().split(" ");
+		System.out.println("Ingrese el número de productos a manejar");
+		int num_productos = Integer.parseInt(input.nextLine());
+		Producto[] productos = new Producto[num_productos];
 		
-		Producto p = new Producto(Integer.parseInt(data_producto[0]), Integer.parseInt(data_producto[1]), Integer.parseInt(data_producto[2]), Integer.parseInt(data_producto[3]));
-		if(p.solicitarPedido()) {
-			System.out.println("Alerta! Solicitar pedido!");
-		}		
+		for(int i = 0; i < num_productos; i++) {
+			System.out.println("Ingrese los datos del producto (Código, precio, cantidad bodega, cantidad mínima)");
+			String[] data_producto = input.nextLine().split(" ");			
+			Producto p = new Producto(Integer.parseInt(data_producto[0]), Integer.parseInt(data_producto[1]), Integer.parseInt(data_producto[2]), Integer.parseInt(data_producto[3]));
+			productos[i] = p;
+		}
+		
+		for(int j = 0; j < productos.length; j++) {
+			if(productos[j].solicitarPedido()) {
+				System.out.println("Alerta! Se debe solicitar pedido al producto "+ productos[j].getCodigo());
+			}
+		}
+		
+		Producto menor = null;
+		int cant_menor = 1000000000;
+		for(int j = 0; j < productos.length; j++) {
+			if(productos[j].getCant_bodega() < cant_menor) {
+				cant_menor = productos[j].getCant_bodega();
+				menor = productos[j];
+			}
+		}
+		System.out.println("El producto menor es: "+menor.getCodigo());
+		
+			
 		
 		input.close();
 	}
