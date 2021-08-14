f5dc8f57d3f1e316f9c62f1cd1fafa5963c5f088
Camilo Cervantes <cacesa8931@gmail.com>
2021-08-13 19:11:47 -0500

    código componente práctico 13

---
+++ src/cprX/LoginController.java
@@ -0,0 +1,24 @@
+package cprX;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private Button login_btn;
+
+    @FXML
+    private TextField user;
+
+    @FXML
+    private TextField pwd;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+}
