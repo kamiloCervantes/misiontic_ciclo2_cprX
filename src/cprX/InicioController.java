a622496678221f92a129aa3f5d32768e6fcab4c2
Camilo Cervantes <cacesa8931@gmail.com>
2021-08-13 19:24:14 -0500

    código componente práctico 14

---
+++ src/cprX/InicioController.java
@@ -0,0 +1,5 @@
+package cprX;
+
+public class InicioController {
+
+}
--- src/cprX/LoginController.java
+++ src/cprX/LoginController.java
@@ -1,24 +1,66 @@
 package cprX;
 
+import java.io.IOException;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class LoginController {
+	
+	@FXML
+	private Button login_btn;
+	@FXML
+	private TextField user, pwd;
+	@FXML
+	private Label mensajeError;
+	
+	private String error = "";
+	
+	
+	public void login(ActionEvent event) {
+		String username = this.user.getText();
+		String password = this.pwd.getText();
+		
+		if(username.contains("prueba")) {
+			this.error = "Usuario no válido";
+		}
+		
+		if(password.length() < 6) {
+			this.error = "Contraseña debe contar con más de 6 caracteres. Intente nuevamente";
+		}
+		
+		if(password.equals(username)) {
+			this.error = "Contraseña no puede ser igual a usuario. Intente nuevamente.";
+		}
+		
+		if(this.error != "") {
+			this.mensajeError.setText(this.error);
+		}
+		else {				
+			try {
+				FXMLLoader loader = new FXMLLoader(LoginController.class.getResource("Inicio.fxml")); 
+				Parent root;
+				root = loader.load();
+				InicioController controller = loader.getController();
+				Scene scene = new Scene(root,452,327);
+				Stage stage = new Stage();
+				stage.setScene(scene);
+				stage.show();	
+				this.login_btn.getScene().getWindow().hide();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+		}
 
-    @FXML
-    private Button login_btn;
-
-    @FXML
-    private TextField user;
-
-    @FXML
-    private TextField pwd;
-
-    @FXML
-    void login(ActionEvent event) {
-
-    }
-
+	}
 }
